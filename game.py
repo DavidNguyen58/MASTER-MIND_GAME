ab0a8e5312c57a15878277cd7d0b762ae47e8ead
DavidNguyen58 <138356579+DavidNguyen58@users.noreply.github.com>
2024-03-03 09:58:00 +0000

    Created the first two functions for front-end developer

--- game.py
+++ game.py
@@ -1,5 +1,19 @@
 # FRONT_END 
 
+colours = ["red", "blue", "green", "yellow"]
 # Player 1 can set a goal
+def main():
+    # Prompt the user 1 to put colour order for player 2 to guess
+    player_1 = colour_order()
+    start()
+
+def colour_order():
+    # Prompt the player 1 to insert a correct order of the colours
+    # The function should return a list of object in the form of color{position}. e.g red0
+    raise NotImplementedError
+
+def start():
+    # Generate a title showing that the game is starting now and prompt the second player to play
+    raise NotImplementedError
 
 # Player 2 will need to find out the correct order in the minimum of move with the support of A.I
