bb6f3e8714b6c999a399078f57a6229bc8e6a69b
DavidNguyen58 <138356579+DavidNguyen58@users.noreply.github.com>
2024-03-04 21:00:08 +0000

    Update the backend for the program

--- game.py
+++ game.py
@@ -1,11 +1,16 @@
 # FRONT_END 
-
 colours = ["red", "blue", "green", "yellow"]
 # Player 1 can set a goal
 def main():
     # Prompt the user 1 to put colour order for player 2 to guess
     player_1 = colour_order()
     start()
+    # The game starts here
+    knowledge = None
+    while True:
+        guess()
+
+
 
 def colour_order():
     # Prompt the player 1 to insert a correct order of the colours
@@ -17,3 +22,23 @@ def start():
     raise NotImplementedError
 
 # Player 2 will need to find out the correct order in the minimum of move with the support of A.I
+
+def knowledge_base():
+    # Encode all necessary information we know about the problem.
+    raise NotImplementedError
+
+def new_knowledge(position):
+    # Input is a list of positions for colours
+    # Update the existing knowledge with the new information
+    raise NotImplementedError
+
+def guess():
+    result = []
+    for i in range(4):
+        x = input(f"Position for{colours[i]}: ")
+        result.append(x)
+    return result
+
+
+if __name__ == "__main__":
+    main()
