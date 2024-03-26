a6ab6da914bebbf5f7fa2952603bd164607d06a0
DavidNguyen58 <138356579+DavidNguyen58@users.noreply.github.com>
2024-03-26 15:01:43 +0000

    Updated on how to use the model

--- guide.py
+++ guide.py
@@ -1,20 +1,15 @@
-from logic import *
 # Demonstrating how to use the predefined model
+from logic import *
 
 # Intializing all the symbols we need to use
 A = Symbol("Red is the colour")
 B = Symbol("Yellow is the colour")
 
-# Create a knowledge base. What we know to be True
-# In this particular example, if we know that we have Red colour, and A implies B then surely we will have B.
-knowledge = And(
-    A,
-    Implication(A, B)
-)
+# The knowledge will store all the information we know about the problem
+knowledge = And()
+knowledge.add(A)
+knowledge.add(B)
 
-
-knowledge.add(Implication(B, A))
-# The model checking algorithm will check for entailment.
-# In this case, KB ⊨ B, so it will print out in the terminal the message.
-if model_check(knowledge, B):
-    print(B)
+# The model checking algorithm will check for entailment. In this case, KB ⊨ B, so it will print out in the terminal the message.
+if model_check(knowledge, A):
+    print(A)
